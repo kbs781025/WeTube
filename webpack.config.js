88bfd23edb82bbbdd5466aac198117070c8a113a
kbs781025 <kbs781025@naver.com>
2020-03-15 23:09:57 +0900

    Done WebPack css loading config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,38 @@
 const path = require("path");
+const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
     entry: ENTRY_FILE,
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/, // Not {}
+                use: ExtractCSS.extract([
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugin() { return [autoprefixer({browsers: "cover 99.5%"})]; }
+                        }
+                    },
+                    {
+                        loader:"sass-loader"
+                    }
+                ])
+            }
+        ]
+    },
     output: {
         path: OUTPUT_DIR,
         filename: "bundle.js"
-    }
+    },
+    plugins: [new ExtractCSS("style.css")]
 };
 
 module.exports = config; // Not export
