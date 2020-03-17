3e32f1543d2147f66fa44aa6ad8f7852e56b86c6
kbs781025 <kbs781025@naver.com>
2020-03-17 16:06:38 +0900

    Passport init done.

--- passport.js
+++ passport.js
@@ -1,1 +1,7 @@
 import userModel from "./models/User";
+import passport from "passport";
+
+passport.use(userModel.createStrategy());
+
+passport.serializeUser(userModel.serializeUser());
+passport.deserializeUser(userModel.deserializeUser());
