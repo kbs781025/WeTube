295cb8fcfb54bf4874204b8a00c94a9e4ffdcf96
kbs781025 <kbs781025@naver.com>
2020-03-17 19:31:53 +0900

    Middleware forwards user data.

--- middleWares.js
+++ middleWares.js
@@ -1,16 +1,14 @@
 import routes from "./routes";
-import multer from "multer"
+import multer from "multer";
 
-const multerVideo = multer({dest: "uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localMiddleWare = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 3
-    }
+    res.locals.user = req.user || null;
+    console.log(req.user);
     next();
-}
+};
 
 export const uploadVideo = multerVideo.single("videoFile");
