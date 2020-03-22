56e930ba6b2b855a96ee06a6567b279aafc4a7f1
kbs781025 <kbs781025@naver.com>
2020-03-22 16:53:24 +0900

    added reference to creator

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const commentSchema = mongoose.Schema({
-    text: {type: String, required: "Text is required."},
-    createData: {type: Date, default: Date.now}
-})
+    text: { type: String, required: "Text is required." },
+    createData: { type: Date, default: Date.now },
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+});
 
 const commentModel = mongoose.model("Comment", commentSchema);
 export default commentModel;
