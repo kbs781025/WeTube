9377fd038cfc13dbc67acf1fe0236c5793461671
kbs781025 <kbs781025@naver.com>
2020-03-24 22:33:34 +0900

    adds comment in real time

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,17 @@
 import axios from "axios";
 const commentForm = document.getElementById("jsCommentForm");
+const commentsList = document.getElementById("jsCommentsList");
+const commentsNumber = document.getElementById("jsCommentsNumber");
+
+function addComment(comment) {
+    const list = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = comment;
+    list.appendChild(span);
+    commentsList.appendChild(list);
+
+    commentsNumber.innerHTML = parseInt(commentsNumber.innerHTML) + 1;
+}
 
 async function handleSubmit(event) {
     event.preventDefault();
@@ -8,11 +20,15 @@ async function handleSubmit(event) {
     commentInput.value = "";
 
     const videoId = window.location.href.split("videos")[1];
-    await axios({
+    const response = await axios({
         url: `/api${videoId}/comment`,
         method: "POST",
         data: { comment }
     });
+
+    if (response.status == 200) {
+        addComment(comment);
+    }
 }
 
 function init() {
